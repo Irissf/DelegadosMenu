b88a84bdfb90bc28ebe98ccafa7a3a17d2547091
Irissf <iris.seijo@gmail.com>
2020-11-05 13:51:42 +0100

    Agregar archivos de proyecto.

---
+++ DelegadosMenu/DelegaAll.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosMenu
+{
+    delegate void MyDelegate();
+    class DelegaAll
+    {
+        public void MenuGenerator(string[] nameOptions, MyDelegate[] menuFunctions)
+        {
+            int option = 0;
+            int final = nameOptions.Length + 1;//si array tiene 3, es 4 por salida
+            string exitName = "Exit";
+
+            do
+            {
+                Console.WriteLine("Choose an option, please:");
+
+                for (int i = 0; i < nameOptions.Length; i++)
+                {
+                    Console.WriteLine("{0}{1,5}", i + 1, nameOptions[i]);
+                }
+
+                Console.WriteLine("{0}{1,5}", final, exitName);
+                try
+                {
+                    option = Convert.ToInt16(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Must be a number!");
+                }
+
+                if (option != final)
+                {
+                    menuFunctions[option - 1].Invoke();
+                }
+                else
+                {
+                    Console.WriteLine("Hasta la vista");
+                }
+
+            } while (option != final);
+
+
+        }
+    }
+}
---
+++ DelegadosMenu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegadosMenu
+{
+    class Program
+    {
+        static void f1()
+        {
+            Console.WriteLine("A");
+        }
+        static void f2()
+        {
+            Console.WriteLine("B");
+        }
+        static void f3()
+        {
+            Console.WriteLine("C");
+        }
+        static void Main(string[] args)
+        {
+            DelegaAll delegaAll = new DelegaAll();
+            delegaAll.MenuGenerator(new string[] { "Op1", "Op2", "Op3" },
+            new MyDelegate[] { f1, f2, f3 });
+            Console.ReadKey();
+        }
+
+    }
+}
